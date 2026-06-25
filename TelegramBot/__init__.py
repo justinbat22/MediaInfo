d3eb4fb99a8419c9ef1b4927a22ac486a3974556
justinbat22 <justinbat22@gmail.com>
2026-06-25 11:12:33 +0530

    Update __init__.py

--- TelegramBot/__init__.py
+++ TelegramBot/__init__.py
@@ -81,7 +81,7 @@ scheduler.start()
 LOGGER(__name__).info("initiating the client....")
 plugins = dict(root="TelegramBot/plugins")
 bot = Client(
-    "TelegramBot",
+    "test",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
